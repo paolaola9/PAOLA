ffb5b8a045bb12ba5a2758b384d5fad9d16ab14a
paolaola <paolaola9@gmail.com>
2018-08-31 15:16:54 -0500

    subiendo2

---
+++ main.php
@@ -0,0 +1,12 @@
+<?php
+$destino="paolalola9@gmail.com"
+$name=$_POST["Name"];
+$Phone=$_POST["Phone"];
+$Email=$_POST["Email "];
+$contenido="Nombre" - $name -  "\nPhone " - $Phone  -  "\nEmail " -$Email";
+mail($destino, "contacto" , $contenido);
+header("location:"index.html");
+
+
+
+?>
