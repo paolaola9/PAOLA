653df4dc42c4573470cdb80eddeb611d033ea288
paolaola <paolaola9@gmail.com>
2018-08-31 15:33:11 -0500

    suebiendo corregido

--- main.php
+++ main.php
@@ -5,7 +5,7 @@ $Phone=$_POST["Phone"];
 $Email=$_POST["Email "];
 $contenido="Nombre" - $name -  "\nPhone " - $Phone  -  "\nEmail " -$Email";
 mail($destino, "contacto" , $contenido);
-header("location:"index.html");
+header("location:index.html");
 
 
 
