1b4305b8a206303c7f63a2a3548d13ca6425bf28
paolaola <paolaola9@gmail.com>
2018-08-31 15:40:37 -0500

    subiendo correciones

--- main.php
+++ main.php
@@ -3,7 +3,7 @@ $destino="paolaola9@gmail.com"
 $name=$_POST["Name"];
 $Phone=$_POST["Phone"];
 $Email=$_POST["Email "];
-$contenido="Nombre" - $name -  "\nPhone " - $Phone  -  "\nEmail " -$Email";
+$contenido="Name: ".$Name."\nPhone: ".$Phone."\nEmail: ".$Email;
 mail($destino, "contacto" , $contenido);
 header("location:index.html");
 
