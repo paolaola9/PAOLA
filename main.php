22e694ae3f10e50e0d825b536933317671aae29e
paolaola <paolaola9@gmail.com>
2018-08-31 16:15:59 -0500

    subien

--- main.php
+++ main.php
@@ -1,9 +1,9 @@
 <?php
 $destino="paolaola9@gmail.com"
-$name=$_POST["Name"];
+$Name=$_POST["Name"];
 $Phone=$_POST["Phone"];
 $Email=$_POST["Email "];
-$contenido="Name: ".$Name."\nPhone: ".$Phone."\nEmail: ".$Email;
+$contenido="Name: "$Name"- \nPhone: "$Phone" - \nEmail: "$Email;
 mail($destino, "contacto" , $contenido);
 header("location:index.html");
 
